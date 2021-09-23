5cb81aede937ace6edcd9637b0833b5844fcd314
Tom Dietel <tom@dietel.net>
2021-09-23 11:30:29 +0200

    Improve trdbox CLI command

--- src/dcs/trdbox.py
+++ src/dcs/trdbox.py
@@ -2,44 +2,40 @@
 import click
 import zmq
 
+class TrdboxCommand:
+    def __init__(self, connect):
+
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REQ)
+        self.socket.connect(connect)
+
+    def exec(self, cmd):
+        self.socket.send_string(cmd)
+        return self.socket.recv_string()
+
+
 @click.group()
 @click.option('-c', '--connect', default='tcp://localhost:7766')
 @click.pass_context
 def trdbox(ctx, connect):
-    ctx.ensure_object(dict)
-
-    ctx.obj['context'] = zmq.Context()
-    ctx.obj['socket'] = ctx.obj['context'].socket(zmq.REQ)
-    ctx.obj['socket'].connect(connect)
+    ctx.obj = TrdboxCommand(connect)
 
 
 @trdbox.command()
 @click.argument('cmd', default=1)
 @click.pass_context
 def pretrigger(ctx, cmd):
-    socket = ctx.obj['socket']
-
-    socket.send_string(f"write 0x08 {cmd}")
-    reply = socket.recv_string()
-    print(reply)
+    ctx.obj.exec(f"write 0x08 {cmd}")
 
 @trdbox.command()
 @click.argument('address', callback=lambda c,p,x: int(x,0))
 @click.pass_context
 def read(ctx, address):
-    socket = ctx.obj['socket']
-
-    socket.send_string(f"read {address}")
-    reply = socket.recv_string()
-    print(reply)
+    print(ctx.obj.exec(f"read {address}"))
 
 @trdbox.command()
 @click.argument('address', callback=lambda c,p,x: int(x,0))
 @click.argument('data', callback=lambda c,p,x: int(x,0))
 @click.pass_context
 def write(ctx, address, data):
-    socket = ctx.obj['socket']
-
-    socket.send_string(f"write {address} {data}")
-    reply = socket.recv_string()
-    print(reply)
+    ctx.obj.exec(f"write {address} {cmd}")
